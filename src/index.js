a6de60c01098d82aa8da6d2c97732061cf3dbbb2
DJ1 <jimmynoble604@gmail.com>
2023-01-23 16:36:04 -0500

    images, footer link, contact and top content complete

--- src/index.js
+++ src/index.js
@@ -39,4 +39,34 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
-console.log('project wired!')
+// Images
+const logoImg = document.querySelector('#logo-img')
+logoImg.src = siteContent.images['logo-img']
+
+const ctaImg = document.querySelector('#cta-img')
+ctaImg.src = siteContent.images['cta-img']
+
+const midImg = document.querySelector('#middle-img')
+midImg.setAttribute('src', siteContent.images['accent-img'])
+
+// Footer link
+const footerLink = document.querySelector('footer a')
+footerLink.textContent = siteContent.footer.copyright
+footerLink.classList.add('bold')
+
+// contact
+const contact = document.querySelector('section.contact')
+contact.children [0].textContent = siteContent.contact['contact-h4']
+contact.children [1].textContent = siteContent.contact['address']
+contact.children [2].textContent = siteContent.contact['phone']
+contact.children [3].textContent = siteContent.contact['email']
+
+// top content
+const topContent = document.querySelector('.top-content')
+topContent.children[0].children[0].textContent = siteContent['main-content']['features-h4']
+topContent.children[0].children[1].textContent = siteContent['main-content']['features-content']
+topContent.children[1].children[0].textContent = siteContent['main-content']['about-h4']
+topContent.children[1].children[1].textContent = siteContent['main-content']['about-content']
+
+// bottom content
+const bottomContent = document.querySelector('.bottom-content')
